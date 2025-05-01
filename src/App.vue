b71bab7143f8df945ee56366a87d9a2137af7258
linkers17 <romeo-gry@mail.ru>
2025-05-01 11:48:49 +0700

    1-basic get date now (2)

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
+<template>
+  <span>Текущая дата: {{ dateNow }}</span>
+</template>
+
 <script setup>
 const dateNow = new Intl.DateTimeFormat('ru-RU', {
   dateStyle: 'full',
 }).format(new Date())
 </script>
-
-<template>
-  <span>Текущая дата: {{ dateNow }}</span>
-</template>
 
 <style scoped></style>
