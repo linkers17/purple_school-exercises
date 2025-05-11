7c00ffe6bf3c43c014636df42ae7d4f1c2af1a8d
linkers17 <romeo-gry@mail.ru>
2025-05-11 16:02:48 +0700

    5-templates

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,26 +1,58 @@
 <template>
 	<div class="card">
-		<div class="card-wrapper">
-			<div class="card-wrapper-number">01</div>
-			<div class="card-wrapper-word">{{ word }}</div>
-			<button class="card-wrapper-rotate" @click="emit('rotate')">Перевернуть</button>
+		<div class="card-wrapper" :class="`card-wrapper_${state}`">
+			<div class="card-wrapper-number">{{ number }}</div>
+			<template v-if="state === 'closed'">
+				<div class="card-wrapper-word">{{ word }}</div>
+				<button class="card-wrapper-rotate" @click="emit('rotate', id, 'opened')">Перевернуть</button>
+			</template>
+			<template v-else>
+				<div class="card-wrapper-word">{{ translation }}</div>
+				<button v-if="isEnded" class="card-wrapper-rotate" @click="emit('rotate', id, 'closed')">Завершено</button>
+				<div v-else class="card-wrapper-actions">
+					<button class="card-wrapper-button-icon" @click="emit('statusChange', id, 'failed')">
+						<FailedIcon />
+					</button>
+					<button class="card-wrapper-button-icon" @click="emit('statusChange', id, 'success')">
+						<SuccessIcon />
+					</button>
+				</div>
+				<FailedIcon v-if="status === 'failed'" class="card-wrapper-status" />
+				<SuccessIcon v-if="status === 'success'" class="card-wrapper-status" />
+			</template>
 		</div>
 	</div>
 </template>
 
 <script setup>
+import {computed} from "vue";
+import FailedIcon from "@/components/icons/FailedIcon.vue";
+import SuccessIcon from "@/components/icons/SuccessIcon.vue";
+
 const emit = defineEmits({
-	rotate: null,
-	statusChange(status) {
-		return status
+	rotate(id) {
+		return id
+	},
+	statusChange(id, status) {
+		return [id, status]
 	},
 })
 
-const { state, status, translation, word } = defineProps({
+const { id, state, status, translation, word } = defineProps({
+	id: Number,
 	state: String,
 	status: String,
 	translation: String,
 	word: String,
+})
+
+const number = computed(() => {
+	if (id < 10) return `0${id}`
+	return `${id}`
+})
+
+const isEnded = computed(() => {
+	return status === 'success' || status === 'failed'
 })
 </script>
 
@@ -77,5 +109,34 @@ const { state, status, translation, word } = defineProps({
 	cursor: pointer;
 	text-transform: uppercase;
 	padding: 0;
+}
+
+.card-wrapper-actions {
+	position: absolute;
+	background-color: var(--color-white);
+	bottom: -12px;
+	left: 50%;
+	transform: translateX(-50%);
+	display: flex;
+	gap: 32px;
+}
+
+.card-wrapper-button-icon {
+	background-color: transparent;
+	border: none;
+	cursor: pointer;
+	height: 24px;
+	width: 24px;
+	padding: 0;
+}
+
+.card-wrapper-status {
+	background-color: var(--color-white);
+	height: 36px;
+	width: 36px;
+	position: absolute;
+	top: -18px;
+	left: 50%;
+	transform: translateX(-50%);
 }
 </style>
